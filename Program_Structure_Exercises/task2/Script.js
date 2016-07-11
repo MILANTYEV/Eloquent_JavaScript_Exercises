7cc3372d6c2daaa87506d3c4cf2f92b4728f1fcc
mihaPetruhin12451@mail.ru <mihaPetruhin12451@mail.ru>
2016-07-12 02:17:03 +0400

    Exercises from "Program structure" chapter.

---
+++ Program_Structure_Exercises/task2/Script.js
@@ -0,0 +1,16 @@
+﻿var amount = Number(prompt("Enter the amount of numbers", ""));
+for (var i = 1; i <= amount; i++) {
+    if ((i % 3 == 0) & (i % 5 == 0)) {
+        document.write("FizzBuzz");
+    }
+    else if (i % 3 == 0) {
+        document.write("Fizz");
+    }
+    else if (i % 5 == 0) {
+        document.write("Buzz");
+    }
+    else {
+        document.write(i);
+    }
+    document.write(" ");
+}
---
+++ Program_Structure_Exercises/task3/Script.js
@@ -0,0 +1,14 @@
+﻿var boardSize = Number(prompt("Enter the size of board", ""));
+for (var i = 0; i < boardSize * boardSize; i++) {
+    if ((i % (boardSize + 1)) == 0) {
+        document.write("<br>");
+        continue;
+    }
+    else if (i % 2 == 0) {
+        document.write("_");
+        continue;
+    }
+    else {
+        document.write("#");
+    }
+}
