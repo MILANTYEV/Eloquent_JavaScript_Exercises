a258a19f01f4aaf498efeebd3ab0899d7a430df6
Fedor <FedyaevOlolo@gmail.com>
2016-07-14 02:21:02 +0400

    Exercises from "Higher order functions" chapter

---
+++ Higher_Order_Functions_Exercises/Task_1_Script.js
@@ -0,0 +1,5 @@
+﻿var array = [[1, 2, 3], [4, 5], [6]];
+
+document.write(array.reduce(function (arr, current) {
+    return arr.concat(current);
+}));
---
+++ Higher_Order_Functions_Exercises/Task_4_Script.js
@@ -0,0 +1,21 @@
+﻿var arr = [1, 2, 3, NaN];
+document.write(every(arr, isNaN) + "<br>");
+document.write(some(arr, isNaN) + "<br>");
+
+function every(arr, func) {
+    for (var i = 0; i <= arr.length; i++) {
+        if (!func(arr[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function some(arr, func) {
+    for (var i = 0; i <= arr.length; i++) {
+        if (func(arr[i])) {
+            return true
+        }
+    }
+    return false;
+}
