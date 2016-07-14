ed5b55aff806cf6979eca1a7e23204faca2af99c
Fedor <FedyaevOlolo@gmail.com>
2016-07-14 21:39:58 +0400

    Exercises from "The secret life of objects" chapter.

---
+++ The_Secret_life_of_objects/Task_1_Script.js
@@ -0,0 +1,18 @@
+﻿function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Vector.prototype.plus = function (vect) {
+    return new Vector((this.x + vect.x), (this.y + vect.y));
+}
+Vector.prototype.minus = function (vect) {
+    return new Vector((this.x - vect.x), (this.y - vect.y));
+}
+Object.defineProperty(Vector.prototype, "length", {
+    get: function () { return Math.sqrt(this.x * this.x + this.y * this.y); }
+});
+
+var vector = new Vector(2, 2);
+var vector2 = new Vector(1, 1);
+document.write(vector.plus(vector2).length + "<br>");
+document.write(vector.minus(vector2).length + "<br>");
---
+++ The_Secret_life_of_objects/Task_3_Script.js
@@ -0,0 +1,31 @@
+﻿function Sequence(arr) {
+    this.arr = arr;
+}
+Sequence.prototype.curent = -1;
+Sequence.prototype.next = function () {
+    if (this.curent == this.arr.length - 1) {
+        document.write("This is last element");
+        return;
+    }
+    this.curent++;
+    return this.arr[this.curent];
+};
+
+function RangeSeq(from, to) {
+    this.arr = [];
+    for (var i = from; i < to; i++) {
+        this.arr.push(i);
+    }
+}
+RangeSeq.prototype = Object.create(Sequence.prototype);
+
+function writeN(seq, N) {
+    for (var i = 1; i <= N; i++) {
+        document.write(seq.next() + "<br>")
+    }
+}
+
+var seq = new Sequence([1, 2, 3, 4, 5, 6]);
+writeN(seq, 4);
+var rangeSeq = new RangeSeq(1, 6);
+writeN(rangeSeq, 4);
